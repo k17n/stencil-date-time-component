dea2b3fec489c131fc39dba2b86bfd987401a7bd
Kn1ghtF0x <karthik.venkataraman96@gmail.com>
2021-05-01 00:53:45 +0530

    Clock rendering issue fix
    Analog clock was not rendering for cases where the time unit has single digits (eg. 1:13:04 PM) due to substring splitting issue. The splitting logic has been modified and `indexOf(':')` has been used to find the proper split index instead of manually hardcoding substring start and stop values.

--- src/components/analog-clock/analog-clock.tsx
+++ src/components/analog-clock/analog-clock.tsx
@@ -18,15 +18,17 @@ export class analogClock {
     private locale: string = Intl.DateTimeFormat().resolvedOptions().locale;
 
     private getClockHandStyles(time: number, timeZone: string): any {
-        
+
         // Get current locale time based on given timezone
         const currentLocaleTimeString = new Date(time).toLocaleTimeString(this.locale, { timeZone: timeZone });
 
-        // Get the hours, minutes and seconds from the date string
-        const hours = parseInt(currentLocaleTimeString.substring(0,2));
-        const minutes = parseInt(currentLocaleTimeString.substring(3,5));
-        const seconds = parseInt(currentLocaleTimeString.substring(6,8));
-        console.log(`hours Minutes Seconds: ${hours},${minutes},${seconds}`);
+        // Format the locale time string by splitting and padding different time units. Value is returned as JSON.
+        const formattedLocaleTime = this.formatTime(currentLocaleTimeString);
+
+        // Convert the hours, minutes and seconds into numbers from string in formattedLocaleTime JSON
+        const hours = parseInt(formattedLocaleTime.hours);
+        const minutes = parseInt(formattedLocaleTime.minutes);
+        const seconds = parseInt(formattedLocaleTime.seconds);
 
         // Calculating the rotating fraction --> how many fraction to rotate for each hand.
         const secondsFraction = seconds / 60;
@@ -39,13 +41,43 @@ export class analogClock {
         const hoursRotate = hoursFraction * 360;
 
         // Generate CSS Transform style
-        let clockHandStyles = {
+        const clockHandStyles = {
             secondHand: `rotate(${secondsRotate}deg)`,
             minuteHand: `rotate(${minutesRotate}deg)`,
             hourHand: `rotate(${hoursRotate}deg)`
         }
 
         return clockHandStyles;
+    }
+
+    private formatTime(timeString: string) {
+        // Occurence of first ':' in the time string
+        const firstSplitIndex = timeString.indexOf(':')
+
+        // Occurence of second ':' in the time string
+        const secondSplitIndex = timeString.indexOf(':', firstSplitIndex + 1)
+
+        // Occurence of space ' ' in the time string. It will be used to delimit the string and omit the AM/PM part
+        const delimitIndex = timeString.indexOf(' ');
+        
+        let formattedTime = {
+            hours: '',
+            minutes: '',
+            seconds: ''
+        }
+        formattedTime.hours = this.padTimeString(timeString.substring(0, firstSplitIndex));
+        formattedTime.minutes = this.padTimeString(timeString.substring(firstSplitIndex + 1, secondSplitIndex));
+        formattedTime.seconds = this.padTimeString(timeString.substring(secondSplitIndex + 1, delimitIndex));
+        return formattedTime;
+    }
+
+    private padTimeString(timeString: string) {
+        if (timeString.length == 1) {
+            return `0${timeString}`
+        }
+        else {
+            return timeString
+        }
     }
 
     render() {
